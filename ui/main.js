f4b7d4b7cfe4b114aeb5b2c736e574665ce9f701
Nasreen-Salma <ernasreensalma@gmail.com>
2017-02-16 23:06:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,14 @@
 console.log('Loaded!');
+
+//changing the inner html
 var element=document.getElementById('main-text');
 element.innerHTML='New-value';
 
+//move the image
+var img=document.getElementById('madi');
+img.onclick=function(){
+  madi.style.marginLeft='100px';  
+  
+    
+    
+};
